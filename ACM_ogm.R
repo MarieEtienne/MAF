1aabb45d8a0f57cc333ba93fffba464dbbbc2dfd
Marie-Pierre Etienne <marie-pierre.etienne@ensai.fr>
2024-12-19 14:05:47 +0100

    update ACM_ogm.R

--- ACM_ogm.R
+++ ACM_ogm.R
@@ -17,6 +17,17 @@ summary(ogm_dta[,1:15])
 
 ## tableau disjoinctif 
 dta_disjonct <- tab.disjonctif(ogm_dta[, 1:15])
+dim(dta_disjonct)
+
+# Résumé des variables actives dans l'ACM
+summary(ogm_dta[,1:15])
+
+
+p <- ncol(ogm_dta[,1:15])
+dta_disjonct |> as_tibble() |> mutate(across(where(is.numeric), function(z){z/p})) |> 
+  summarise(across(where(is.numeric), mean))
+
+mod_freq <- dta_disjonct |> as_tibble() |> summarise(across(where(is.numeric), mean)) 
 
 ogm_dta <- ogm_dta %>%
   mutate(`Position Al H`= recode(`Position Al H`, 'Très Favorable'='Favorable')) |> 
@@ -28,11 +39,11 @@ summary(ogm_dta[,1:15])
 summary(ogm_dta[,16:21])
 
 
-res <- MCA(ogm_dta, ncp = 5, quali.sup = 17:21, graph = FALSE)
+res <- MCA(ogm_dta, ncp = 5, quali.sup = 16:21, graph = FALSE)
 
 fviz_eig(res, choice = "variance")
 
-fviz_mca_ind(res, invisible = c("var", "quali.sup"), label = "none")
+fviz_mca_ind(res, invisible = c("var", "quali.sup"), label = "none", axes = c(1,2))
 
 fviz_mca_ind(res, invisible = c("var", "quali.sup"), label = "none", select.ind = list(cos2 = 0.5))
 
@@ -46,8 +57,13 @@ fviz_mca_var(res, col.quali.sup="#FF4D00", invisible = c( "ind", "var"))
 fviz_mca(res, invisible = c( "quali.sup"), label = "var", select.ind = list(cos2 = 0.5), select.var = list(contrib = 10))
 
 
+fviz_mca(res, invisible = c( "ind", "var"))
+
+
 fviz_contrib(res, choice = "var", axes = 1)
 
 fviz_contrib(res, choice = "var", axes = 2)
 
 fviz_cos2(res, choice = "var")
+
+dimdesc(res)
